d8bd4e6273e47058200c5ae9148385115abb3caa
Nathan <nathanmcneil@pm.me>
2020-06-26 13:43:41 -0700

    more routes, added PassportJS

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,29 @@
 const router = require('express').Router()
 
- < going to write routes later>
+const router = require('express').Router()
+const { User } = require('../models')
+const jwt = require('jsonwebtoken')
+const passport = require('passport')
 
-  module.exports = router
+// Register Route
+router.post('/users/register', (req, res) => {
+ const { name, email, username } = req.body
+ User.register(new User({ name, email, username }), req.body.password, err => {
+  if (err) { console.error(err) }
+  res.sendStatus(200)
+ })
+})
+
+// Login Route
+router.post('/users/login', (req, res) => {
+ User.authenticate()(req.body.username, req.body.password, (err, user) => {
+  if (err) { console.error(err) }
+  res.json(user ? jwt.sign({ id: user._id }, process.env.SECRET) : null)
+ })
+})
+
+router.get('/users/posts', passport.authenticate('jwt'), (req, res) => {
+ res.json(req.user)
+})
+
+module.exports = router
