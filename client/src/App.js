2893b15cd517068684e627bdc75068e19ddd6ccd
Michael <tironamichael@gmail.com>
2020-06-30 14:09:45 -0700

    Added routes for sign up and login page in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,33 @@
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
 import 'antd/dist/antd.css'
 import Login from './pages/Login'
 import SignUp from './pages/SignUp'
 
 const App = () => {
   return(
-    <>
-    <Login />
-    <div>
-      hello world
-    </div>
-    </>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path='/'>
+            <SignUp />
+          </Route>
+          <Route path='/Login'>
+            <Login />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+    // <>
+    // <Login />
+    // <div>
+    //   hello world
+    // </div>
+    // </Router>
   )
 }
 
