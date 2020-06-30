a70c493287b054fc89d9266a018a23ff67a01622
Erika <erika.paige.handley@gmail.com>
2020-06-30 11:24:39 -0700

    basic login form

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import 'antd/dist/antd.css'
+import Login from './pages/Login'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  return(
+    <>
+    <Login />
+    <div>
+      hello world
     </div>
-  );
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ client/src/pages/Login/Login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import LoginForm from '../../components/LoginForm'
+
+const Login = () => {
+  
+  return(
+    <>
+      <LoginForm />
+    </>
+  )
+}
+
+export default Login
