01921d96fd1f5921316b4283468c800d9a245217
Jonathan Liang <jonathanliang2690123@gmail.com>
2020-07-06 10:15:48 -0700

    added in template (changes in progress)

--- client/src/pages/AboutUs/AboutUs.js
+++ client/src/pages/AboutUs/AboutUs.js
@@ -1,15 +1,41 @@
-import React from 'react'
+import React from "react";
+// nodejs library that concatenates classes
+import classNames from "classnames";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+// @material-ui/icons
+import Camera from "@material-ui/icons/Camera";
+import Palette from "@material-ui/icons/Palette";
+import Favorite from "@material-ui/icons/Favorite";
+
+// core components
 import Navbar from '../../components/Navbar'
 import Footer from '../../components/Footer'
 
-const AboutUs =()=>
+/*
+import Header from "components/Header/Header.js";
+import Footer from "components/Footer/Footer.js";
+import Button from "components/CustomButtons/Button.js";
+import GridContainer from "components/Grid/GridContainer.js";
+import GridItem from "components/Grid/GridItem.js";
+import HeaderLinks from "components/Header/HeaderLinks.js";
+import NavPills from "components/NavPills/NavPills.js";
+import Parallax from "components/Parallax/Parallax.js";
+*/
+
+
+
+const AboutUs = () =>
 {
     return
     (
-        <>
-        <Navbar/>
-        <Footer/>
-        </>
+        <div>
+            <Navbar/>
+
+
+
+            <Footer/>
+        </div>
     );
 }
 
